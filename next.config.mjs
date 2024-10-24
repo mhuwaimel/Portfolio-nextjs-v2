98a2f69e4ff01f99c3fdc4d5d2830f1cb0f3b382
Mohammed Aziz <66377368+mhuwaimel@users.noreply.github.com>
2024-10-24 10:18:22 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "assets.aceternity.com"],
-  },
+ 
 };
 
 export default nextConfig;
